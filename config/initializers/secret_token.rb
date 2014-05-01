52da79f992687b66d828e38bd307001a37d4de70
mrjusepelo <jperezlondono@starenweb.com>
2014-04-30 21:47:13 -0500

    deploy initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HerragroDev::Application.config.secret_key_base = 'e5aa00f67919eba0b68a705630d0fb1e6659138c22b0f55cd4afe271a4ebb02d563f8dd02d8605c16c48898f943b1e69302369342e6a9239cf7a11b492f3b816'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HerragroDev::Application.config.session_store :cookie_store, key: '_herragro_dev_session'
